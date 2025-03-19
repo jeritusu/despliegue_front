19e58ea61ea88681fed4e5a709c8cb9ab1711045
Jairo Usuay <95385835+jeritusu@users.noreply.github.com>
2025-03-19 18:30:52 -0500

    Update app.js
    agregue URL de front

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const backendURL = "https://backend-36z7.onrender.com"; // Cambia por la URL de tu backend
+const backendURL = "https://despliegue-zkpx.onrender.com"; // Cambia por la URL de tu backend
 
 // Obtener los items
 async function fetchItems() {
